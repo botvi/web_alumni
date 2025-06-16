bdb52cfe9eda5e91c8a37dd28af3bc949f578e3c
Yovi Ardiansyah <younoob07mei@gmail.com>
2025-06-16 14:18:15 +0700

    main

--- resources/views/pageweb/daftar_kegiatan_alumni/index.blade.php
+++ resources/views/pageweb/daftar_kegiatan_alumni/index.blade.php
@@ -11,24 +11,23 @@
         <h2>Daftar Kegiatan Alumni</h2>
     </div><!-- End Section Title -->
 
-    <div class="container">
-
-        <div class="row gy-4">
+    <div class="container py-5">
+        <div class="row g-4">
             @foreach ($kegiatan as $item)
             <div class="col-lg-4 col-md-6" data-aos="fade-up" data-aos-delay="100">
-                <div class="service-item position-relative">
-                    <div class="text-center mb-3">
+                <div class="service-item position-relative h-100 shadow-sm rounded">
+                    <div class="text-center p-3">
                         <img src="{{ asset('uploads/kegiatan/'.$item->gambar) }}" alt="Gambar Kegiatan" class="img-fluid rounded" style="width: 100%; height: 200px; object-fit: cover;">
                     </div>
-                    <div class="p-3">
+                    <div class="p-4">
                         <h4 class="text-center mb-3">Kegiatan Alumni</h4>
                         <div class="mb-3">
                             <i class="bi bi-geo-alt-fill text-primary"></i>
                             <span class="ms-2">{{ $item->tempat }}</span>
                         </div>
-                        <p class="text-center">{!! Str::limit($item->deskripsi_kegiatan, 100) !!}</p>
-                        <div class="text-center mt-3">
-                            <a href="#" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#modalKegiatan{{ $item->id }}">Baca Selengkapnya</a>
+                        <p class="text-center mb-4">{!! Str::limit($item->deskripsi_kegiatan, 100) !!}</p>
+                        <div class="text-center">
+                            <a href="#" class="btn btn-primary px-4" data-bs-toggle="modal" data-bs-target="#modalKegiatan{{ $item->id }}">Baca Selengkapnya</a>
                         </div>
                     </div>
                 </div>
@@ -42,7 +41,7 @@
                             <h5 class="modal-title" id="modalKegiatanLabel{{ $item->id }}">Detail Kegiatan Alumni</h5>
                             <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                         </div>
-                        <div class="modal-body">
+                        <div class="modal-body p-4">
                             <div class="text-center mb-4">
                                 <img src="{{ asset('uploads/kegiatan/'.$item->gambar) }}" alt="Gambar Kegiatan" class="img-fluid rounded" style="max-height: 400px; object-fit: cover;">
                             </div>
@@ -51,7 +50,7 @@
                                 <i class="bi bi-geo-alt-fill text-primary"></i>
                                 <span class="ms-2">{{ $item->tempat }}</span>
                             </div>
-                            <p>{!! $item->deskripsi_kegiatan !!}</p>
+                            <p class="mb-0">{!! $item->deskripsi_kegiatan !!}</p>
                         </div>
                         <div class="modal-footer">
                             <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Tutup</button>
@@ -61,7 +60,6 @@
             </div>
             @endforeach
         </div>
-
     </div>
 
 </section><!-- /Services Section -->
